984e16772169d5f39d4ae2847f6f9eadde307ce0
Robert Weißgraeber <robert.weissgraeber@ax-semantics.com>
2023-12-04 11:33:31 +0100

    Updated from Publii

--- wom/data/definition.js
+++ wom/data/definition.js
@@ -295,8 +295,6 @@ const design = ["default.css","buttons-colors-on.css", "progressbar.css"];
 	********************************************************************
 
 	const addons = ["extras/addon_results_textfilter_by_button.js"]
-
-
 	const addons = ["extras/addon_results_textfilter_by_button.js", "extras/addon_check_iframe_resize_client.js", "extras/addon_limit_results.js", "extras/addon_favorite_party.js", "extras/addon_show_first_results.js"]
 	const addons = []
 	const addons = ["my_folder/my_file.js"]	
